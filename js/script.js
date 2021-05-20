7a8e3417c659e5ee2c80d428fb12a16ef8e6b6d1
InfinitydeGuzma <>
2021-05-20 03:38:49 +0000

    added const for variables

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@
 function doMathClicked () {
   // this code will check if you guessed correctly or not
 
-  userNumber = parseInt(document.getElementById('user-number').value)
+  const userNumber = parseInt(document.getElementById('user-number').value)
 
-  answer = Math.floor(Math.random() * 6) + 1;  // returns a random integer from 1 to 6
+  const randomNumber = Math.floor(Math.random() * 6) + 1  // returns a random integer from 1 to 6
 
-  if (answer == userNumber) {
-    document.getElementById('answer').innerHTML = "You guessed correctly! Great job!";
+  if (randomNumber === userNumber) {
+    document.getElementById('answer').innerHTML = "You guessed correctly! Great job!"
   }
 
-  if (answer !== userNumber) {
-    document.getElementById('answer').innerHTML = "Sorry, you got it wrong. Try again :(";
+  if (randomNumber !== userNumber) {
+    document.getElementById('answer').innerHTML = "Sorry, you got it wrong. Try again :("
   }
 }
